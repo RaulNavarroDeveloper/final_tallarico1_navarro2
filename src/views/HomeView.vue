38655fcd549ed16429cb47a183d12761bfdfaa53
RaulNavarroDeveloper <84648327+RaulNavarroDeveloper@users.noreply.github.com>
2022-07-26 18:33:32 -0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,20 @@ export default {
   name: 'HomeView',
   components: {
     HomeComponentes
+  },
+  data(){
+      return {
+        items: [],
+      }
+  },
+  mounted(){
+    fetch('https://sodasstereo.000webhostapp.com/api/todos.php')
+    .then(response => response.json())
+    .then(response => {
+      this.items = response;
+      localStorage.setItem('prueba', JSON.stringify(this.items));
+      console.log(this.items);
+    })
   }
 }
 </script>
