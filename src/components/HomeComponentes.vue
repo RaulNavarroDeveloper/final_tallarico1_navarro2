bd6d08f8e05bcc78faac36fb37824e80857d0665
RaulNavarroDeveloper <84648327+RaulNavarroDeveloper@users.noreply.github.com>
2022-07-26 18:41:28 -0300

    Update HomeComponentes.vue

--- src/components/HomeComponentes.vue
+++ src/components/HomeComponentes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fondo">
-    <h1 class="pt-2">"GRACIAS"</h1>
+    <h1 class="pt-2">"GRACIAS TOTALES"</h1>
     <p class="fs-4 text-blanco">Te invitamos a conocer más sobre la banda que logró conquistar a millones de personas</p>
   </div>
 </template>
